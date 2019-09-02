bd42e4afd264cee6b0e2ea6617a87efe33648202
Anthony Cerbic <kuzzdra@gmail.com>
2019-09-02 23:36:11 +0300

    initial

---
+++ index.ts
@@ -0,0 +1,8 @@
+/**
+ * Ant.Design Button that toggles user login/logout state
+ */
+export { ButtonLoginGoogle } from "./components/ButtonLoginGoogle";
+/**
+ * "Receptacle" page hook that allows to read results of Login after final redirection
+ */
+export { useLoginSuccess } from "./components/useLoginSuccess";
