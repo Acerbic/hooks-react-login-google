258db3e4ac2d753373b29ff6b3ae58d66ed507d5
Anthony Cerbic <kuzzdra@gmail.com>
2019-09-05 22:50:40 +0300

    simplefy loginSuccess hook

--- src/components/useLoginSuccess.ts
+++ src/components/useLoginSuccess.ts
@@ -1,48 +1,26 @@
 /**
  * A custom hook to check for authentication token in URL search params string.
- * Returns a state object with two flags:
- * {
- *   loading: boolean; // is true until the check is done.
- *   logSuccess: boolean; // holds results of checking for URL params, after
- *                        // loading turns false.
- * }
+ * Returns a boolean value that indicates login success.
  */
 
-import { useState, useEffect } from "react";
 import { set as lsSet } from "local-storage";
 
 export const useLoginSuccess = function(
     storage_field_name: string = "login-with-google-token",
     query_param_name: string = "authtoken"
-) {
-    let [loading, changeLoading] = useState(true);
-    let [logSuccess, changeLogSuccess] = useState(false);
-
-    /**
-     * One-time on mounting: check params for auth token
-     */
-    useEffect(() => {
-        changeLoading(false);
-
-        // process token from login by API server.
-
-        const query: string | false =
-            typeof window !== "undefined" &&
-            window.location &&
-            window.location.search;
-        if (query) {
-            const urlParams = new URLSearchParams(query);
-            if (urlParams.has(query_param_name)) {
-                changeLogSuccess(true);
-                lsSet(storage_field_name, urlParams.get(query_param_name));
-            }
+): boolean {
+    const query: string | false =
+        typeof window !== "undefined" &&
+        window.location &&
+        window.location.search;
+    if (query) {
+        const urlParams = new URLSearchParams(query);
+        if (urlParams.has(query_param_name)) {
+            lsSet(storage_field_name, urlParams.get(query_param_name));
+            return true;
         }
-    }, []);
-
-    return {
-        loading,
-        logSuccess,
-    };
+    }
+    return false;
 };
 
 export default useLoginSuccess;
